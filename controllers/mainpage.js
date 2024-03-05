0b837ad82a2c2977590fc4f137e407682c5b79ce
kanchan2812 <dighekanchan4@gmail.com>
2024-03-05 12:55:18 +0530

    first commit

---
+++ controllers/mainpage.js
@@ -0,0 +1,7 @@
+
+exports.gethomePage = (request, response, next) => {
+    response.sendFile('home.html', { root: 'views' });
+}
+exports.geterrorPage = (request,response,next) =>{
+    response.sendFile('notFound.html',{root:'views'});
+}
